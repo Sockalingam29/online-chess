7892bc1be76d70da76de0c8a4fd5cee9e7fc9db4
HARIMUTHU-M <harimuthutce@gmail.com>
2022-10-16 20:03:28 +0530

    Co-authored-by: Sockalingam <Sockalingam29@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Game, move, status, moves, aiMove, getFen } from "js-chess-engine";
 
 function App() {
+  const [boardArray2, setBoardArray2] = useState({});
+  const game = new Game();
+  let keys = Object.keys(boardArray2);
+  console.log(game.exportJson());
+  let board = game.exportJson().pieces;
+
+  useEffect(() => {
+    // console.log(board);
+
+    // let boardArray = {};
+    // console.log(board);
+
+    setCurrBoard();
+  }, []);
+
   function setCurrBoard() {
+    let boardArray = {};
     let xaxis = {
       1: "A",
       2: "B",
@@ -25,6 +41,7 @@ function App() {
     };
 
     for (let i = 1; i <= 8; i++) {
+      board = game.exportJson().pieces;
       for (let j = 1; j <= 8; j++) {
         let piece = board[xaxis[i] + yaxis[j]];
         if (piece) {
@@ -34,27 +51,30 @@ function App() {
         }
       }
     }
+    setBoardArray2({ ...boardArray });
+    keys = Object.keys(boardArray2);
+
+    console.log();
   }
 
-  const game = new Game();
-  // game.printToConsole();
-  // const [currBoard, setCurrBoard] = useState([{}, {}]);
-
-  let board = game.board.configuration.pieces;
-  // console.log(board);
-
-  let boardArray = {};
-  setCurrBoard();
-  // console.log(board);
-
   const [possibleMoves, setPossibleMoves] = useState([]);
-  const tempFn = (x, y) => {
-    console.log(x + " " + y);
-    setPossibleMoves(game.moves(x));
-    console.log(possibleMoves);
-  };
+  const [fromPosition, setfromPosition] = useState("");
 
-  const keys = Object.keys(boardArray);
+  const tempFn = (x, y) => {
+    if (possibleMoves.length > 0) {
+      game.move(fromPosition, x);
+
+      setCurrBoard();
+      console.log("Dine");
+      setfromPosition("");
+      setPossibleMoves([]);
+    } else {
+      console.log(x + " " + y);
+      setPossibleMoves(game.moves(x));
+      setfromPosition(x);
+      console.log(possibleMoves);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center h-screen w-screen">
@@ -67,9 +87,9 @@ function App() {
               (possibleMoves.includes(val) && "bg-gray-400")
             }
             // id={val}
-            onClick={() => tempFn(val, boardArray[val])}
+            onClick={() => tempFn(val, boardArray2[val])}
           >
-            {boardArray[val]}
+            {boardArray2[val]}
           </div>
         ))}
       </div>
